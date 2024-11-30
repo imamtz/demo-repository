9a7f20f1fac1428da8b7786ee1f5a3b60a298882
imamtz <61140273+imamtz@users.noreply.github.com>
2024-11-30 09:55:03 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,9 +11,8 @@ def multiply(x, y):  # New function
     return x * y
 
 def divide(a, b):
-    """Divides two numbers and returns the result.
     """
-    """
+    Divides two numbers and returns the result.
     Raise ZeroDivisionError if the second number (y) is zero.
     """
     if y == 0:
